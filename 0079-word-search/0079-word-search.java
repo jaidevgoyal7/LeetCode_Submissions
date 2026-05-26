7610a2d8402a50a853a665becf4c2ac968adcd19
Jaidev Goyal <55891903+jaidevgoyal7@users.noreply.github.com>
2026-05-26 05:46:24 +0530

    Time: 307 ms (5.96%), Space: 47.1 MB (6.21%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,18 +2,16 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         boolean[][] visited = new boolean[board.length][board[0].length];
         for(int row = 0; row < board.length; row++){
-            for(int col = 0; col < board[row].length; col++){
-                if(backtrack( 0, row, col, visited, board, word)){
+            for(int col = 0; col < board[0].length; col++){
+                if(backtrackWithDFS(0, row, col, board, word, visited)){
                     return true;
                 }
             }
         }
         return false;
-        
     }
 
-    public boolean backtrack(int i, int row, int col, boolean[][] visited, char[][] board, String word){
-
+    public boolean backtrackWithDFS(int i, int row, int col, char[][] board, String word, boolean[][] visited){
         int[] rowdir = {0, 0, 1, -1};
         int[] coldir = {1, -1, 0, 0};
 
@@ -21,24 +19,20 @@ class Solution {
             return true;
         }
 
-        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || visited[row][col] || board[row][col] != word.charAt(i)){
+        if(row >= board.length || col >= board[0].length || row < 0 || col < 0 || visited[row][col] || board[row][col] != word.charAt(i)){
             return false;
         }
 
         visited[row][col] = true;
 
-        if(row < board.length && col < board[0].length && row >= 0 && col >= 0 && board[row][col] == word.charAt(i)){
-            
-            for(int d = 0; d < 4; d++){
-                int newrow = row + rowdir[d];
-                int newcol = col + coldir[d];
-                if(backtrack(i+1, newrow, newcol, visited, board, word)){
-                    return true;
-                }
-            }
-            
-        }
+        for(int d = 0; d < 4; d++){
+            int newrow = row + rowdir[d];
+            int newcol = col + coldir[d];
 
+            if(backtrackWithDFS(i+1, newrow, newcol, board, word, visited)){
+                return true;
+            }
+        }
         visited[row][col] = false;
         return false;
 
