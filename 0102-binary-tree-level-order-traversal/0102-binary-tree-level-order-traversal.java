467bf5571b08e45961c8f04edef82b942dcaa3b2
Jaidev Goyal <55891903+jaidevgoyal7@users.noreply.github.com>
2026-05-30 07:53:10 +0530

    Time: 2 ms (19.21%), Space: 46.4 MB (94.57%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -16,6 +16,9 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
         bfs(root, result, new ArrayList<>());
         return result;
     }
